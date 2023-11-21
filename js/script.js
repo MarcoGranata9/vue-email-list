d96160a4ff5af4db98de232188d2939d2f2b8157
MarcoGranata9 <granatam9900@gmail.com>
2023-11-21 16:49:54 +0100

    add: api generated email list

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const {createApp} = Vue
+
+
+
+
+createApp({
+    data() {
+        return {
+            emails: []
+        }
+    },
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
+            console.log(resp.data.response);
+            this.emails.push(resp.data.response)
+            })
+        }
+    } 
+}).mount("#app")
